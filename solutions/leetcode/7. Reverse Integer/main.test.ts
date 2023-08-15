84f3d82af4640205b26abe7fa8e5fd9341a12e87
mikoloism <mikoloism@gmail.com>
2023-08-15 07:00:48 +0330

    test(solutions/leetcode): update test files and test cases with `each` verbose adaptor for `each` vitest api

--- solutions/leetcode/7. Reverse Integer/main.test.ts
+++ solutions/leetcode/7. Reverse Integer/main.test.ts
@@ -1,8 +1,10 @@
-import { describe, test } from '@/vitest.js';
+import { describe, each } from '@/vitest.js';
 import { reverse } from './main.js';
 
 describe('7. Reverse Integer', function testReverseInteger() {
-	test('Case 1').input(123).expect(321).run(reverse);
-	test('Case 2').input(-123).expect(-321).run(reverse);
-	test('Case 3').input(120).expect(21).run(reverse);
+	each([
+		{ name: 'Case 1', input: [123], expect: 321 },
+		{ name: 'Case 2', input: [-123], expect: -321 },
+		{ name: 'Case 3', input: [120], expect: 21 },
+	]).test(reverse);
 });
