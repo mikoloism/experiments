c240fe3fe4675a96dedea2e3248475d0e22717e7
mikoloism <mikoloism@gmail.com>
2023-08-15 06:59:19 +0330

    chore(coverage): install `@vitest/coverage-v8` package. configure vitest for coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,12 @@ import * as path from 'node:path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+	test: {
+		coverage: {
+			provider: 'v8',
+			reporter: ['text', 'json', 'html'],
+		},
+	},
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './common'),
