effa6bcca9bd126c15e323f6f1a8918dab509e94
mikoloism <mikoloism@gmail.com>
2023-08-18 23:06:09 +0330

    style: format files
    formatted with `prettier`\
    via run `yarn format`\
    to execute `yarn prettier . --write` command

--- plopfile.mjs
+++ plopfile.mjs
@@ -7,8 +7,8 @@ import { sort_markdown_table_action } from './scripts/plop/sort_markdown_table.a
 /** @typedef {import('plop').NodePlopAPI} NodePlopAPI */
 /** @param {NodePlopAPI} plop */
 export default function plopfile(plop) {
-	plop.setPartial('SolutionDirectoryPath', solution_directory_path_partial);
-	plop.setActionType('sort_markdown_table', sort_markdown_table_action);
-	plop.setActionType('leetcode_fetch_problem', leetcode_fetch_problem_action);
-	plop.setGenerator('Solution (LeetCode.com)', leetcode_solution_generator);
+    plop.setPartial('SolutionDirectoryPath', solution_directory_path_partial);
+    plop.setActionType('sort_markdown_table', sort_markdown_table_action);
+    plop.setActionType('leetcode_fetch_problem', leetcode_fetch_problem_action);
+    plop.setGenerator('Solution (LeetCode.com)', leetcode_solution_generator);
 }
