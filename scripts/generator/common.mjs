a70df90b8286826fb8f147fe00d6336891a9d787
mikoloism <mikoloism@gmail.com>
2023-08-27 17:48:16 +0330

    ci(common): add custom high-level module around the app of generator
    write app and api modules to run automatically generators

--- scripts/generator/common.mjs
+++ scripts/generator/common.mjs
@@ -2,6 +2,8 @@ import Handlebars from 'handlebars';
 import * as fs from 'node:fs/promises';
 import * as path from 'node:path';
 import puppeteer from 'puppeteer';
+import inquirer from 'inquirer';
+
 function render(template, data, option) {
     Handlebars.registerHelper('is_exist', function (v, o) {
         if (typeof v !== 'undefined') {
@@ -66,4 +68,68 @@ async function writefile(source, content) {
     return await fs.writeFile(source, content, { encoding: 'utf8' });
 }
 
-export { get_page, make_url, render, readfile, writefile };
+class Api {
+    async ask(question) {
+        const answer = await inquirer.prompt(question);
+        return answer[question.name];
+    }
+
+    async run(handler) {
+        return handler.call(null, this);
+    }
+
+    async file(option) {
+        throw '`file` method was not implemented';
+    }
+}
+
+const DEFAULT_OPTION = {
+    message: 'Select Creators -> ',
+};
+
+class App {
+    constructor(option = null) {
+        this.api = new Api();
+        this.option = option || DEFAULT_OPTION;
+    }
+
+    #handlers = {};
+    create(name, handler) {
+        this.#handlers[name] = handler;
+        return this;
+    }
+
+    async #askCreator() {
+        const answers = await inquirer.prompt({
+            type: 'list',
+            name: 'CREATOR_NAME',
+            message: this.option.message,
+            choices: Object.keys(this.#handlers).map((key) => ({
+                name: key,
+                type: 'choice',
+            })),
+        });
+        return answers['CREATOR_NAME'];
+    }
+
+    async #runCreator(creator_name) {
+        const creator_handler = this.#handlers[creator_name];
+        return await creator_handler.call(null, this.api);
+    }
+
+    async end() {
+        try {
+            const creator_name = await this.#askCreator();
+            const result = await this.#runCreator(creator_name);
+            return result;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+}
+
+function defineApp(option = null) {
+    return new App(option);
+}
+
+export { get_page, make_url, render, readfile, writefile, defineApp };
