7972c48545ddfe20e0cf3b72e5faaeec2f142d78
mikoloism <mikoloism@gmail.com>
2023-12-18 18:17:29 +0330

    feat(solution/leetcode): add answer of `12. Integer to Roman`

--- solutions/leetcode/12. Integer to Roman/solution.ts
+++ solutions/leetcode/12. Integer to Roman/solution.ts
@@ -1,3 +1,29 @@
-function intToRoman(num: number): string {}
+const ROMAN_MAP = new Map()
+    .set(1000, 'M')
+    .set(900, 'CM')
+    .set(500, 'D')
+    .set(400, 'CD')
+    .set(100, 'C')
+    .set(90, 'XC')
+    .set(50, 'L')
+    .set(40, 'XL')
+    .set(10, 'X')
+    .set(9, 'IX')
+    .set(5, 'V')
+    .set(4, 'IV')
+    .set(1, 'I');
+
+function intToRoman(num: number): string {
+    let romaned = '';
+
+    for (let [value, symbol] of ROMAN_MAP.entries()) {
+        while (num >= value) {
+            romaned = romaned.concat(symbol);
+            num -= value;
+        }
+    }
+
+    return romaned;
+}
 
 export { intToRoman };
