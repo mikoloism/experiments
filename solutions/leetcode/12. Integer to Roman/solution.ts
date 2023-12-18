bc5831cc49bbe46b70ed8c9361b434a34ec85057
mikoloism <mikoloism@gmail.com>
2023-12-18 18:40:51 +0330

    perf(solution/leetcode): improve `12. Integer to Roman` solution
    - replace Map to Array

--- solutions/leetcode/12. Integer to Roman/solution.ts
+++ solutions/leetcode/12. Integer to Roman/solution.ts
@@ -1,21 +1,22 @@
-const ROMAN_MAP = new Map<number, string>()
-    .set(1000, 'M')
-    .set(900, 'CM')
-    .set(500, 'D')
-    .set(400, 'CD')
-    .set(100, 'C')
-    .set(90, 'XC')
-    .set(50, 'L')
-    .set(40, 'XL')
-    .set(10, 'X')
-    .set(9, 'IX')
-    .set(5, 'V')
-    .set(4, 'IV')
-    .set(1, 'I');
+const ROMAN_MAP: Array<[number, string]> = [
+    [1000, 'M'],
+    [900, 'CM'],
+    [500, 'D'],
+    [400, 'CD'],
+    [100, 'C'],
+    [90, 'XC'],
+    [50, 'L'],
+    [40, 'XL'],
+    [10, 'X'],
+    [9, 'IX'],
+    [5, 'V'],
+    [4, 'IV'],
+    [1, 'I'],
+];
 
 function intToRoman(num: number): string {
     let romaned = '';
-    for (let [value, symbol] of ROMAN_MAP.entries()) {
+    for (let [value, symbol] of ROMAN_MAP) {
         let count = Math.floor(num / value);
         romaned = romaned.concat(symbol.repeat(count));
         num -= value * count;
