aec53fa01983760f57638bbfac792ab09f8604da
mikoloism <mikoloism@gmail.com>
2023-12-18 18:48:55 +0330

    perf(solution/leetcode): improve `12. Integer to Roman`

--- solutions/leetcode/12. Integer to Roman/solution.ts
+++ solutions/leetcode/12. Integer to Roman/solution.ts
@@ -1,27 +1,14 @@
-const ROMAN_MAP: Array<[number, string]> = [
-    [1000, 'M'],
-    [900, 'CM'],
-    [500, 'D'],
-    [400, 'CD'],
-    [100, 'C'],
-    [90, 'XC'],
-    [50, 'L'],
-    [40, 'XL'],
-    [10, 'X'],
-    [9, 'IX'],
-    [5, 'V'],
-    [4, 'IV'],
-    [1, 'I'],
-];
+const ONES = ['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'];
+const TENS = ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'];
+const HNDS = ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'];
+const THSD = ['', 'M', 'MM', 'MMM'];
 
 function intToRoman(num: number): string {
-    let romaned = '';
-    for (let [value, symbol] of ROMAN_MAP) {
-        let count = Math.floor(num / value);
-        romaned = romaned.concat(symbol.repeat(count));
-        num -= value * count;
-    }
-    return romaned;
+    return ''
+        .concat(THSD[Math.floor(num / 1000)])
+        .concat(HNDS[Math.floor((num % 1000) / 100)])
+        .concat(TENS[Math.floor((num % 100) / 10)])
+        .concat(ONES[num % 10]);
 }
 
 export { intToRoman };
